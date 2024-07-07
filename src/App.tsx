5749f4d8c6c124105733a2f518f58ad80490f77f
sewakgautam <sewak.gautam58@gmail.com>
2024-07-07 20:53:20 +0100

    project init

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import "./App.css";
+
+import {
+  GithubFilled,
+  CodepenCircleFilled,
+  LinkedinFilled,
+} from "@ant-design/icons";
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="leftPanel">
+          <div className="upperLeftPanel">
+            <p className="title">Sewak Gautam</p>
+            <p className="designation">Full Stack Developer</p>
+            <p className="subtitle">Curious about what's happening? <br/>By the way, he’s not yet a full stack developer. Let him complete this first, and then we can consider it.</p>
+            <p className="subtitle">A new year brings a new place and a new version, with exciting development underway !!!</p>
+          </div>
+          <div className="lowerLeftPanel">
+          <p className="subtitle">Ignore these icons; they are not going to work! !!!</p>
+            <a>
+              <GithubFilled />
+              <LinkedinFilled />
+              <CodepenCircleFilled />
+            </a>
+          </div>
+        </div>
+        <div className="RightPanel">
+          <p>
+            Wanna go back to Previous Version ?{" "}
+            <a href="https://v2.sewakgautam.com.np" target="_blank">
+              version 1.0
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
