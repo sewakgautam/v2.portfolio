799d5d62142906901155ac984aacd00a06cda249
sewakgautam <sewak.gautam58@gmail.com>
2024-07-07 22:19:49 +0100

    glow added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import {
@@ -6,6 +7,19 @@ import {
   LinkedinFilled,
 } from "@ant-design/icons";
 function App() {
+  const [glowPosition, setGlowPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e: any) => {
+      setGlowPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
   return (
     <>
       <div className="container">
@@ -13,11 +27,20 @@ function App() {
           <div className="upperLeftPanel">
             <p className="title">Sewak Gautam</p>
             <p className="designation">Full Stack Developer</p>
-            <p className="subtitle">Curious about what's happening? <br/>By the way, he’s not yet a full stack developer. Let him complete this first, and then we can consider it.</p>
-            <p className="subtitle">A new year brings a new place and a new version, with exciting development underway !!!</p>
+            <p className="subtitle">
+              Curious about what's happening? <br />
+              By the way, he’s not yet a full stack developer. Let him complete
+              this first, and then we can consider it.
+            </p>
+            <p className="subtitle">
+              A new year brings a new place and a new version, with exciting
+              development underway !!!
+            </p>
           </div>
           <div className="lowerLeftPanel">
-          <p className="subtitle">Ignore these icons; they are not going to work! !!!</p>
+            <p className="subtitle">
+              Ignore these icons; they are not going to work! !!!
+            </p>
             <a>
               <GithubFilled />
               <LinkedinFilled />
@@ -25,7 +48,7 @@ function App() {
             </a>
           </div>
         </div>
-        <div className="RightPanel">
+        <div className="rightPanel">
           <p>
             Wanna go back to Previous Version ?{" "}
             <a href="https://v2.sewakgautam.com.np" target="_blank">
@@ -34,6 +57,13 @@ function App() {
           </p>
         </div>
       </div>
+      <div
+        className="glow"
+        style={{
+          top: `${glowPosition.y}px`,
+          left: `${glowPosition.x}px`,
+        }}
+      ></div>
     </>
   );
 }
