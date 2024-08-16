9244922b28375dfa736ad37c7336c2f863455cf2
sewakgautam <sewak.gautam58@gmail.com>
2024-08-16 22:38:14 +0100

    some worth changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,58 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import melogo from "./assets/meSewak.png";
 
-import {
-  GithubFilled,
-  CodepenCircleFilled,
-  LinkedinFilled,
-} from "@ant-design/icons";
 function App() {
-  const [glowPosition, setGlowPosition] = useState({ x: 0, y: 0 });
+  const [textIndex, setTextIndex] = useState(0);
+  const textArray = [
+    "Node.Js",
+    "React",
+    "Python",
+    "Javascript",
+    "Figma",
+    "Matlab",
+    "Html",
+    "Css",
+    "ant.d",
+  ];
 
   useEffect(() => {
-    const handleMouseMove = (e: any) => {
-      setGlowPosition({ x: e.clientX, y: e.clientY });
-    };
+    const interval = setInterval(() => {
+      setTextIndex((prevIndex) => (prevIndex + 1) % textArray.length);
+    }, 5000);
 
-    window.addEventListener("mousemove", handleMouseMove);
+    return () => clearInterval(interval);
+  }, [textArray.length]);
 
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
   return (
     <>
       <div className="container">
-        <div className="leftPanel">
-          <div className="upperLeftPanel">
-            <p className="title">Sewak Gautam</p>
-            <p className="designation">Full Stack Developer</p>
-            <p className="subtitle">
-              Curious about what's happening? <br />
-              By the way, he’s not yet a full stack developer. Let him complete
-              this first, and then we can consider it.
-            </p>
-            <p className="subtitle">
-              A new year brings a new place and a new version, with exciting
-              development underway !!!
-            </p>
+        <div className="content">
+          <div className="text-overlay">
+            <h1 key={textArray[textIndex]} className="background-text">
+              {textArray[textIndex]}
+            </h1>
           </div>
-          <div className="lowerLeftPanel">
-            <p className="subtitle">
-              Ignore these icons; they are not going to work! !!!
-            </p>
-            <a>
-              <GithubFilled />
-              <LinkedinFilled />
-              <CodepenCircleFilled />
-            </a>
+          <div className="introContent">
+            <img className="swk" src={melogo}></img>
+
+            <div className="intro">{/* <h1>Sewak Gautam .</h1> */}</div>
           </div>
         </div>
-        <div className="rightPanel">
+        <p className="flirt">
+          No worries at all, but just a heads-up—we’re in the middle of a major
+          site upgrade right now. I promise it’ll be worth the wait! 😉 But if
+          it’s super urgent, you know where to find me. I’m just a message away,
+          ready to help in any way I can. Don’t be shy!
+        </p>
+        <a href="https://v2.sewakgautam.com.np">
           <p>
-            Wanna go back to Previous Version ?{" "}
-            <a href="https://v2.sewakgautam.com.np" target="_blank">
-              version 1.0
-            </a>
+            By the way, you can always revisit the old site if you’re feeling
+            nostalgic.
           </p>
-        </div>
+        </a>{" "}
+        <p>Go ahead and click above ☝️ , I promise it'll be worth it! 😉</p>
       </div>
-      <div
-        className="glow"
-        style={{
-          top: `${glowPosition.y}px`,
-          left: `${glowPosition.x}px`,
-        }}
-      ></div>
     </>
   );
 }
